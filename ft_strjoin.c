83c76dc2722e577ec61a1eeff359af99ec907e14
AlejandroJimenez16 <alejanjiga@gmail.com>
2024-12-28 14:28:21 +0100

    Def

---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejanjiga <alejanjiga@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 14:03:53 by alejandj          #+#    #+#             */
+/*   Updated: 2024/12/28 14:27:36 by alejanjiga       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    size_t i;
+    size_t j;
+    char *join;
+
+    if (!s1 || !s2)
+       return (NULL); 
+    join = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+    if (!join)
+        return (NULL);
+    i = 0;
+    while (s1[i] != '\0')
+    {
+        join[i] = s1[i];
+        i++;
+    }
+    j = 0;
+    while (s2[j] != '\0')
+    {
+        join[i] = s2[j];
+        i++;
+        j++;
+    }
+    join[i] = '\0';
+    return (join);
+}
