f7e96a71155e34dc2054859d5cbc42bc17da0907
Alejandro Jimenez Garcia <alejandj@c2r12s2.42madrid.com>
2025-01-20 00:59:58 +0100

    Parte obligatoria hecha

--- libft.h
+++ libft.h
@@ -33,7 +33,7 @@ char *ft_strdup(const char *s);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
-//char **ft_split(char const *s, char c); EN PROCESO
+char **ft_split(char const *s, char c);
 char *ft_itoa(int n);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char*));
@@ -42,9 +42,9 @@ void ft_putstr_fd(char *s, int fd);
 void ft_putendl_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
 
-//=============================================
-//                   BONUS                 
-//=============================================
+/*=============================================*/
+/*                   BONUS                     */
+/*=============================================*/
 
 typedef struct		s_list
 {
