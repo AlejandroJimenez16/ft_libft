fdfc96e48b1f695dfb2d5900291e95114b7554c1
Alejandro Jimenez Garcia <alejandj@c2r10s4.42madrid.com>
2025-01-22 14:30:06 +0100

    Perfect

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 16:30:47 by alejandj          #+#    #+#             */
-/*   Updated: 2025/01/21 17:12:03 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/01/22 12:16:34 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 /**
  * @brief Safely copies a block of memory, handling overlaps.
- * 
- * This function copies `n` bytes from the source memory block `src` to the 
+ *
+ * This function copies `n` bytes from the source memory block `src` to the
  * destination memory block `dest`. Unlike `memcpy`, `memmove` is designed
  * to handle overlapping memory regions safely by ensuring the data is not
  * corrupted during the copy process. It archieves this by temporarily buffering
  * the data if the source and destination regions overlap.
- * 
+ *
  * Use `memmove` when the source and destination memory regions may overlap.
- * For non-overlapping memory regions, `memcpy` is faster and should 
+ * For non-overlapping memory regions, `memcpy` is faster and should
  * be preferred.
- * 
- * @param dest Pointer to the destination memory block where the content 
+ *
+ * @param dest Pointer to the destination memory block where the content
  * will be copied.
  * @param src Pointer to the source memory block to copy from.
  * @param n The number of bytes to copy.
@@ -33,26 +33,27 @@
  */
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t				i;
-	unsigned char		*d;
-	unsigned char		*s;
-	unsigned char		temp[n];
+	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
 
-	i = 0;
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	if (!dest && !src)
 		return (NULL);
-	while (i < n)
-	{
-		temp[i] = s[i];
-		i++;
-	}
 	i = 0;
-	while (i < n)
+	if (d > s)
 	{
-		d[i] = temp[i];
-		i++;
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	else
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
 	}
 	return (dest);
 }
