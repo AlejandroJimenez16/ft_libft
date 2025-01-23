09adca17d31e6ce9accb42895f87cf2d1858fd4a
Alejandro Jimenez Garcia <alejandj@c2r7s5.42madrid.com>
2025-01-23 14:05:31 +0100

    Listas

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:18:56 by alejandj          #+#    #+#             */
-/*   Updated: 2025/01/22 16:56:45 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/01/23 12:50:24 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*node;
 	t_list	*new_node;
+	void	*mapped_content;
 
 	new_lst = NULL;
 	node = lst;
+	if (!lst || !f || !del)
+		return (NULL);
 	while (node != NULL)
 	{
-		new_node = ft_lstnew(f(node->content));
+		mapped_content = f(node->content);
+		new_node = ft_lstnew(mapped_content);
 		if (!new_node)
 		{
+			del(mapped_content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
